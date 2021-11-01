2f48c517bf72186bea8728a25979cf4673b81290
Fehat Ozkasgari <ferhat@dice.fm>
2021-11-01 12:24:02 -0400

    jwt-auth

--- api/views.py
+++ api/views.py
@@ -1,2 +1,31 @@
 from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+
+from api.permissions import IsOwner
+
+
+class APIRootView(APIView):
+    """
+    Root of backed api
+    This is the actual api endpoint for all api calls
+    """
+    def get(self, request, format=None):
+        return Response({
+            'hello': reverse('hello', request=request, format=format),
+            'token': reverse('token_obtain_pair', request=request, format=format),
+            'token/refresh': reverse('token_refresh', request=request, format=format),
+            'token/verify': reverse('token_verify', request=request, format=format),
+        })
+
+
+class HelloView(APIView):
+    permission_classes = [IsAuthenticated, IsOwner]
+
+    def get(self, request):
+        content = {
+            'message': 'Hello, World!',
+            'owner': 'kelepirci',
+        }
+        return Response(content)
---
+++ backend/views.py
@@ -0,0 +1,15 @@
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework.views import APIView
+
+
+class RootView(APIView):
+    """
+    Root of beckend
+    """
+    def get(self, request, *args, **kwargs):
+        return Response({
+            'admin': request.build_absolute_uri('admin/'),
+            'auth': request.build_absolute_uri('auth/'),
+            'api': request.build_absolute_uri('api/'),
+        })
